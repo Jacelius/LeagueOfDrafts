95d4fbec00434fdb930b41f5e2125b34e2b9524c
Silas Paaske <sipn@itu.dk>
2023-05-07 20:28:43 +0200

    deleted dist, use public

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'public',
+  },
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
 });
